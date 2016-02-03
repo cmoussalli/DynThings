5efbc62c950d512b9914a37bd2a48c2d8dd51834
Caesar Moussalli <cmoussalli@gmail.com>
2016-02-03 18:12:15 +0400

    Add Default Values Script

--- DynThingsDB/Add Default Data.sql
+++ DynThingsDB/Add Default Data.sql
@@ -3,8 +3,8 @@
 truncate table [dbo].[DBSettings]
 
 Insert into [dbo].[DBSettings] (ID,Title,Valu) values 
-(1,N'Database Version',N'0.01')
+(1,N'Database Version',N'0.01'),
+(2,N'Development Mode',N'1')
 
 
---Fill the Dev Test Values
 
