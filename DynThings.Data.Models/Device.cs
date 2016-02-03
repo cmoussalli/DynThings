312646e5ebb61404a82bfbe6c7a5a0b033202d9c
Caesar Moussalli <cmoussalli@gmail.com>
2016-02-03 13:38:32 +0400

    Add Create Methods to Device Extenstion Model

--- DynThings.Data.Models/Device.cs
+++ DynThings.Data.Models/Device.cs
@@ -25,6 +25,9 @@ namespace DynThings.Data.Models
         public long ID { get; set; }
         public string Title { get; set; }
         public Nullable<long> StatusID { get; set; }
+        public Nullable<System.Guid> GUID { get; set; }
+        public Nullable<System.Guid> KeyPass { get; set; }
+        public string PinCode { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<DeviceCommand> DeviceCommands { get; set; }
