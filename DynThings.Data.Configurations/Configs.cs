8f2b4f8c842a5b72755113a6da7fc408d8643951
Caesar Moussalli <cmoussalli@gmail.com>
2016-02-03 13:37:37 +0400

    Implement Dynthings.Data.Config

---
+++ DynThings.Data.Configurations/Configs.cs
@@ -0,0 +1,61 @@
+﻿/////////////////////////////////////////////////////////////////
+// Created by : Caesar Moussalli                               //
+// TimeStamp  : 3-2-2016                                       //
+// Content    : Handle the database configuration              //
+// Notes      :                                                //
+//                                                             //
+/////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynThings.Data;
+using DynThings.Data.Models;
+
+namespace DynThings.Data.Configurations
+{
+    public class Config
+    {
+        #region DB Context
+        private static DynThingsEntities db = new DynThingsEntities();
+        #endregion
+
+        /// <summary>
+        /// Return the develoment mode activation.
+        /// </summary>
+        public bool developementMode
+        {
+            get
+            {
+                bool result = false;
+                DBSetting dbset = db.DBSettings.Find(2);
+                if (dbset.valu == "1")
+                {
+                    result = true;
+                }
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Return the Database version
+        /// </summary>
+        public float DBVersion
+        {
+            get
+            {
+                float result = 0;
+                DBSetting dbset = db.DBSettings.Find(1);
+                result = float.Parse(dbset.valu);
+                return result;
+            }
+        }
+
+
+       
+    }
+
+
+}
