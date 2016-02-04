d502b04c781a868d4a39e6a01577c19479e4bcdb
Caesar Moussalli <cmoussalli@gmail.com>
2016-02-04 07:07:35 +0400

    Implement Device Controller

---
+++ DynThings.WebPortal/Controllers/DevicesController.cs
@@ -0,0 +1,157 @@
+﻿/////////////////////////////////////////////////////////////////
+// Created by : Caesar Moussalli                               //
+// TimeStamp  : 31-1-2016                                      //
+// Content    : Handel the Device Actions                      //
+// Notes      :                                                //
+//                                                             //
+/////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DynThings.Data.Models;
+using PagedList;
+
+namespace DynThings.WebPortal.Controllers
+{
+    public class DevicesController : Controller
+    {
+        private DynThingsEntities db = new DynThingsEntities();
+
+        // GET: Devices
+        public ActionResult Index()
+        {
+            var devices = db.Devices.Include(d => d.DeviceStatu);
+            return View(devices.ToList());
+        }
+
+        // GET: Devices/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Devices.Find(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // GET: Devices/Create
+        public ActionResult Create()
+        {
+            ViewBag.StatusID = new SelectList(db.DeviceStatus, "ID", "Title");
+            return View();
+        }
+
+        // POST: Devices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,StatusID,GUID,KeyPass,PinCode")] Device device)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Devices.Add(device);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StatusID = new SelectList(db.DeviceStatus, "ID", "Title", device.StatusID);
+            return View(device);
+        }
+
+        // GET: Devices/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Devices.Find(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StatusID = new SelectList(db.DeviceStatus, "ID", "Title", device.StatusID);
+            return View(device);
+        }
+
+        // POST: Devices/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,StatusID,GUID,KeyPass,PinCode")] Device device)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(device).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StatusID = new SelectList(db.DeviceStatus, "ID", "Title", device.StatusID);
+            return View(device);
+        }
+
+        // GET: Devices/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Devices.Find(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: Devices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Device device = db.Devices.Find(id);
+            db.Devices.Remove(device);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+
+
+
+        [HttpGet]
+        public PartialViewResult GetListPV(string searchfor = null, int page = 1, int recordsperpage = 2)
+        {
+            PagedList.IPagedList devs = db.Devices
+                .Where(e => searchfor == null || e.Title.Contains(searchfor))
+                .OrderBy(e => e.Title).ToList()
+                .ToPagedList(page, recordsperpage);
+
+
+            return PartialView("_List", devs);
+        }
+    }
+}
