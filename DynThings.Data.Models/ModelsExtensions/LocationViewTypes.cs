5764642f0bb3ab162b38932a32a9b5e99b798c52
Caesar Moussalli <cmoussalli@gmail.com>
2016-02-16 00:02:19 +0400

    Implement LocationView Repository

---
+++ DynThings.Data.Models/ModelsExtensions/LocationViewTypes.cs
@@ -0,0 +1,19 @@
+﻿///////////////////////////////////////////////////////////////////
+// Created by : Caesar Moussalli                                 //
+// TimeStamp  : 15-2-2016                                        //
+// Content    : Extend the properties of LocationViewTypes Model //
+// Notes      : Don't add Behavior in this class                 //
+///////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynThings.Data.Models
+{
+    public partial class LocationViewTypes
+    {
+        private DynThingsEntities db = new DynThingsEntities();
+    }
+}
---
+++ DynThings.Data.Repositories/Repositories/LocationViewTypesRepository.cs
@@ -0,0 +1,35 @@
+﻿/////////////////////////////////////////////////////////////////
+// Created by : Caesar Moussalli                               //
+// TimeStamp  : 15-2-2016                                      //
+// Content    : Handle LocationViewTypes CRUD                  //
+// Notes      :                                                //
+/////////////////////////////////////////////////////////////////
+using DynThings.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynThings.Data.Repositories
+{
+    public class LocationViewTypesRepository
+    {
+        private DynThingsEntities db = new DynThingsEntities();
+
+        #region GetList
+        /// <summary>
+        /// Get list of LocationView Types
+        /// </summary>
+        /// <returns>List of LocationView Types </returns>
+        public List<LocationViewType> GetList()
+        {
+            List<LocationViewType> locViewTypes = db.LocationViewTypes.ToList();
+            return locViewTypes;
+        }
+        #endregion
+
+
+
+    }
+}
