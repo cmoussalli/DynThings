c5c74c07fe7be1ac56971abce484f30cdac1516b
Caesar Moussalli <cmoussalli@gmail.com>
2016-02-16 14:15:12 +0400

    Implement EndPointCommand Annotation

---
+++ DynThings.Data.Models/ModelsExtensions/EndPointCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynThings.Data.Models
+{
+    public partial class EndPointCommand
+    {
+        private DynThingsEntities db = new DynThingsEntities();
+
+    }
+}
