b09ef895490b9663e94bbfd317e4457efffda4a6
Caesar Moussalli <cmoussalli@gmail.com>
2016-02-16 12:42:34 +0400

    Fix ValidateRecordsPerPage

---
+++ DynThings.WebPortal/Helpers/Configs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynThings.Data.Configurations;
+
+namespace DynThings.WebPortal.Helpers
+{
+    public static class Configs
+    {
+        public static int validateRecordsPerPage(int pageInput)
+        {
+            int result = pageInput;
+            if (pageInput == 0)
+            {
+                result = Config.DefaultRecordsPerPage;
+            }
+            return result;
+        }
+    }
+}
