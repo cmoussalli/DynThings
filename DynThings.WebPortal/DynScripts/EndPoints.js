cd93f788848e6a25e9ee38a28873238d4867b4dd
Caesar Moussalli <cmoussalli@gmail.com>
2016-02-29 22:31:22 +0400

    Move Endpoints JS to EndPoint.js

---
+++ DynThings.WebPortal/DynScripts/EndPoints.js
@@ -0,0 +1,43 @@
+﻿function LoadPart_EndPointListDiv() {
+    var loadingpart = LoadDivLoading();
+    $("#divEndPointsList").html(loadingpart);
+    $.ajax({
+        url: getRootURL() + '/EndPoints/ListPV?searchfor=' + $(txtEndPointSearch).val() + '&recordsperpage=0',
+        //page=" + $("#DynConfigCurrentPage").html,
+        type: "GET",
+    })
+        .done(function (partialViewResult) {
+            $("#divEndPointsList").html(partialViewResult);
+        });
+    return false;
+};
+
+$(document).ready(function () {
+    $(document).on("click", "#EndPointsListPager a[href]", function () {
+        var loadingpart = LoadDivLoading();
+        $("#divEndPointsList").html(loadingpart);
+        $.ajax({
+            url: $(this).attr("href") + "&searchfor=" + $(txtEndPointSearch).val() + '&recordsperpage=0',
+            type: 'GET',
+            cache: false,
+            success: function (result) {
+                $("#divEndPointsList").html(result);
+                return false;
+            }
+        });
+        return false;
+    });
+});
+
+function LoadPart_DialogEndPointAdd() {
+    var loadingpart = LoadDivLoading();
+    $("#modal").html(loadingpart);
+    $.ajax({
+        url: getRootURL() + '/EndPoints/addpv',
+        //page=" + $("#DynConfigCurrentPage").html,
+        type: "GET",
+    })
+    .done(function (partialViewResult) {
+        $("#modal").html(partialViewResult);
+    });
+}
