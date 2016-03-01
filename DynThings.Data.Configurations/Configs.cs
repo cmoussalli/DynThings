59708ae3debbeacdaa8296399ca49b07dd6a71dd
Arshad Ashraf <a4arshad46@gmail.com>
2016-03-01 18:37:54 +0400

    Test Github commit

--- DynThings.Data.Configurations/Configs.cs
+++ DynThings.Data.Configurations/Configs.cs
@@ -7,11 +7,7 @@
 /////////////////////////////////////////////////////////////////
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DynThings.Data;
 using DynThings.Data.Models;
 
 namespace DynThings.Data.Configurations
