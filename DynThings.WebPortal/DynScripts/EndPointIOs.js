c926e1b0c8b45c7cc59016f358abe18b61599aef
Caesar Moussalli <cmoussalli@gmail.com>
2016-03-30 14:04:09 +0400

    Fix Paging in EndpointIOs

--- DynThings.WebPortal/DynScripts/EndPointIOs.js
+++ DynThings.WebPortal/DynScripts/EndPointIOs.js
@@ -7,12 +7,32 @@ function LoadPart_EndPointIOsListDiv() {
     var drpdwnIOTypeID = document.getElementById("IOTypeID");
     var selectedIOTypeID = drpdwnIOTypeID.options[drpdwnIOTypeID.selectedIndex].value;
     $.ajax({
-        url: getRootURL() + '/EndPointIOs/ListPV?search=' + $(txtEndPointIOsSearch).val() + '&recordsperpage=0' + '&endPointID=' + $(txtEndPointIDSearch).val() + '&ioTypeID=' + selectedIOTypeID,
+        url: getRootURL() + '/EndPointIOs/ListPV?search=' + $(txtEndPointIOsSearch).val() + '&recordsperpage=0' + '&endPointID=' + $(txtEndPointIDSearch).val() + '&ioTypeID=' + $(IOTypeID).val(),
         //page=" + $("#DynConfigCurrentPage").html,
         type: "GET",
     })
         .done(function (partialViewResult) {
             $("#divEndPointIOsList").html(partialViewResult);
+            AttachEventEndPointIOsListPager();
         });
     return false;
 };
+
+
+//Attach : Pager
+function AttachEventEndPointIOsListPager() {
+    $(document).on("click", "#EndPointIOsListPager a[href]", function () {
+        var loadingpart = LoadDivLoading();
+        $("#divEndPointIOsList").html(loadingpart);
+        $.ajax({
+            url: $(this).attr("href") + "&search=" + $(txtEndPointIOsSearch).val() + '&recordsperpage=0' + '&endPointID=' + $(txtEndPointIDSearch).val() + '&ioTypeID=' + $(IOTypeID).val(),
+            type: 'GET',
+            cache: false,
+            success: function (result) {
+                $("#divEndPointIOsList").html(result);
+                return false;
+            }
+        });
+        return false;
+    });
+}
