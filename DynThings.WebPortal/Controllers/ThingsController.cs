176016cff52a586cdcf2e7d1387fd46100108db5
cesar <cesar@CMS>
2016-08-22 14:17:05 +0400

    Implement Date Filter in Monitor Thing History

--- DynThings.WebPortal/Controllers/ThingsController.cs
+++ DynThings.WebPortal/Controllers/ThingsController.cs
@@ -246,7 +246,7 @@ namespace DynThings.WebPortal.Controllers
         [HttpGet]
         public ActionResult Rpt_IOsHistory(long ThingID, long endPointTypeID, DateTime fromDate, DateTime toDate, int page = 1, int recordsperpage = 0)
         {
-            IPagedList IOs = UnitOfWork_Repositories.repoThings.GetThingEndIOs(ThingID, endPointTypeID, fromDate, toDate, page, Helpers.Configs.validateRecordsPerMaster(recordsperpage));
+            IPagedList IOs = UnitOfWork_Repositories.repoThings.GetThingEndIOs(ThingID, endPointTypeID, fromDate, toDate.AddDays(1), page, Helpers.Configs.validateRecordsPerMaster(recordsperpage));
             return PartialView("Reports/Grids/HistoryIOs", IOs);
 
 
