610db232b449ccad3fad6f7ef2adee3de13ddfbb
cmoussalli <cmoussalli@gmail.com>
2017-03-20 11:23:54 +0400

    Add API support: Locations

---
+++ DynThings.WebPortal/Controllers/API/3rdParty/LocationsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DynThings.Data.Models;
+using DynThings.Core;
+using DynThings.WebAPI.Models;
+using DynThings.WebAPI.Repositories;
+
+namespace DynThings.WebPortal.Controllers.API
+{
+    public class LocationsController : ApiController
+    {
+        #region Props
+        UnitOfWork_WebAPI uow_APIs = new UnitOfWork_WebAPI();
+        long entityID = 7;
+
+        #endregion
+
+    
+    }
+}
