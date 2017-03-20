e80064207d5a7dba555f7316c05b24c66befe541
cmoussalli <cmoussalli@gmail.com>
2017-03-20 13:36:10 +0400

    Add APILocationAdapter

---
+++ DynThings.WebAPI.Models/APILocation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynThings.WebAPI.Models
+{
+    public class APILocation
+    {
+        #region :: Public Properties ::
+        public long ID { get; set; }
+        public System.Guid GUID { get; set; }
+        public string Title { get; set; }
+        public string LongitudeY { get; set; }
+        public string LatitudeX { get; set; }
+        public bool isActive { get; set; }
+        public long Status { get; set; }
+        public long IconID { get; set; }
+        #endregion
+
+        #region :: Constructor ::
+        public APILocation()
+        {
+            this.ID = 0;
+            this.GUID = System.Guid.NewGuid();
+            this.Title = "";
+            this.LongitudeY = "";
+            this.LatitudeX = "";
+            this.isActive = false;
+            this.Status = 0;
+            this.IconID = 0;
+
+        }
+        #endregion
+    }
+}
---
+++ DynThings.WebAPI.Models/TypesMapper/APILocationAdapter.cs
@@ -0,0 +1,31 @@
+﻿using DynThings.Data.Models;
+using DynThings.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynThings.WebAPI.Models.TypesMapper
+{
+
+    public static class APILocationAdapter
+    {
+        public static APILocation fromLocation(Location sourceLocation)
+        {
+            APILocation result = new APILocation();
+            result.GUID = sourceLocation.GUID;
+            result.IconID = sourceLocation.IconID;
+            result.ID = sourceLocation.ID;
+            result.isActive = sourceLocation.isActive;
+            result.LatitudeX = sourceLocation.LatitudeX;
+            result.LongitudeY = sourceLocation.LatitudeX;
+            result.Status = sourceLocation.Status;
+            result.Title = sourceLocation.Title;
+
+            return result;
+        }
+    }
+
+
+}
