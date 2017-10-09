6a21a1211a1be4a1f2c0f77eb6d8ccb7c27e4757
cmoussalli <cmoussalli@gmail.com>
2017-10-09 17:09:07 +0400

    Delete the old Database Project

--- DynThings.ServiceConfigurationManager/Form1.Designer.cs
+++ DynThings.ServiceConfigurationManager/Form1.Designer.cs
@@ -28,9 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblServiceStatus = new System.Windows.Forms.Label();
             this.txtServer = new System.Windows.Forms.TextBox();
             this.txtDatabase = new System.Windows.Forms.TextBox();
             this.txtUser = new System.Windows.Forms.TextBox();
@@ -40,82 +39,79 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnService = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(224, 11);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "btnServiceStatus";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Location = new System.Drawing.Point(23, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(79, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Service Status:";
             // 
-            // label2
+            // lblServiceStatus
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(111, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "lblServiceStatus";
+            this.lblServiceStatus.AutoSize = true;
+            this.lblServiceStatus.Location = new System.Drawing.Point(122, 41);
+            this.lblServiceStatus.Name = "lblServiceStatus";
+            this.lblServiceStatus.Size = new System.Drawing.Size(83, 13);
+            this.lblServiceStatus.TabIndex = 2;
+            this.lblServiceStatus.Text = "lblServiceStatus";
             // 
             // txtServer
             // 
-            this.txtServer.Location = new System.Drawing.Point(152, 83);
+            this.txtServer.Location = new System.Drawing.Point(93, 86);
             this.txtServer.Name = "txtServer";
-            this.txtServer.Size = new System.Drawing.Size(100, 20);
+            this.txtServer.Size = new System.Drawing.Size(276, 20);
             this.txtServer.TabIndex = 3;
             this.txtServer.Text = "txtServer";
             // 
             // txtDatabase
             // 
-            this.txtDatabase.Location = new System.Drawing.Point(152, 109);
+            this.txtDatabase.Location = new System.Drawing.Point(93, 112);
             this.txtDatabase.Name = "txtDatabase";
-            this.txtDatabase.Size = new System.Drawing.Size(100, 20);
+            this.txtDatabase.Size = new System.Drawing.Size(276, 20);
             this.txtDatabase.TabIndex = 4;
             this.txtDatabase.Text = "txtDatabase";
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(152, 135);
+            this.txtUser.Location = new System.Drawing.Point(93, 138);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.Size = new System.Drawing.Size(276, 20);
             this.txtUser.TabIndex = 5;
             this.txtUser.Text = "txtUser";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(152, 161);
+            this.txtPassword.Location = new System.Drawing.Point(93, 164);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.Size = new System.Drawing.Size(276, 20);
             this.txtPassword.TabIndex = 6;
             this.txtPassword.Text = "txtPassword";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(272, 215);
+            this.button2.Location = new System.Drawing.Point(102, 14);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 28);
             this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
+            this.button2.Text = "Refresh";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Location = new System.Drawing.Point(12, 89);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 1;
@@ -124,7 +120,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 112);
+            this.label4.Location = new System.Drawing.Point(12, 115);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 1;
@@ -133,7 +129,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 138);
+            this.label5.Location = new System.Drawing.Point(12, 141);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 13);
             this.label5.TabIndex = 1;
@@ -142,42 +138,87 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 164);
+            this.label6.Location = new System.Drawing.Point(12, 167);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(53, 13);
             this.label6.TabIndex = 1;
             this.label6.Text = "Password";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(-9, 201);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(419, 73);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(303, 14);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 28);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnService);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.lblServiceStatus);
+            this.groupBox2.Location = new System.Drawing.Point(-9, -13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(443, 77);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // btnService
+            // 
+            this.btnService.Location = new System.Drawing.Point(286, 36);
+            this.btnService.Name = "btnService";
+            this.btnService.Size = new System.Drawing.Size(92, 23);
+            this.btnService.TabIndex = 3;
+            this.btnService.Text = "btnService";
+            this.btnService.UseVisualStyleBackColor = true;
+            this.btnService.Click += new System.EventHandler(this.btnService_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 305);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(397, 252);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUser);
             this.Controls.Add(this.txtDatabase);
             this.Controls.Add(this.txtServer);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DynThings Service Configuration Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblServiceStatus;
         private System.Windows.Forms.TextBox txtServer;
         private System.Windows.Forms.TextBox txtDatabase;
         private System.Windows.Forms.TextBox txtUser;
@@ -187,6 +228,10 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnService;
     }
 }
 
--- DynThings.ServiceConfigurationManager/Form1.cs
+++ DynThings.ServiceConfigurationManager/Form1.cs
@@ -10,19 +10,25 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Entity;
 using System.Data.Entity.Core.EntityClient;
+using DynThings.Core;
+using System.ServiceProcess;
 
 namespace DynThings.ServiceConfigurationManager
 {
-    class ConnectionInfo
-    {
-        public string Server { get; set; }
-        public string Database { get; set; }
-        public string User { get; set; }
-        public string Password { get; set; }
-    }
+
 
     public partial class Form1 : Form
     {
+        string ServiceName = "DynThings.Service";
+
+        class ConnectionInfo
+        {
+            public string Server { get; set; }
+            public string Database { get; set; }
+            public string User { get; set; }
+            public string Password { get; set; }
+        }
+
         ConnectionInfo connectionInfo = new ConnectionInfo();
 
         public Form1()
@@ -32,22 +38,138 @@ namespace DynThings.ServiceConfigurationManager
 
         private void button2_Click(object sender, EventArgs e)
         {
+            ValidateServiceStatus();
             LoadConfig();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ValidateServiceStatus();
+            LoadConfig();
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveConfig();
+            MessageBox.Show("Configurations has been saved");
+
+        }
+
+        private void btnService_Click(object sender, EventArgs e)
+        {
+            ServiceController sc = new ServiceController(ServiceName);
+            if (sc.Status == ServiceControllerStatus.Stopped)
+            {
+                sc.Start();
+
+            }
+            if (sc.Status == ServiceControllerStatus.Running)
+            {
+                sc.Stop();
+
+            }
+        }
+
+
 
         private void LoadConfig()
         {
             string connectionStr = System.Configuration.ConfigurationManager.ConnectionStrings["DynThingsEntities"].ConnectionString;
-            MessageBox.Show(connectionStr);
 
             EntityConnectionStringBuilder connectionEntity = new EntityConnectionStringBuilder(connectionStr);
             string x = connectionEntity.ProviderConnectionString;
             SqlConnection connectionSQL = new SqlConnection(x);
             connectionInfo.Server = connectionSQL.DataSource;
             connectionInfo.Database = connectionSQL.Database;
-            connectionInfo.User = connectionSQL.Credential.UserId;
-            connectionInfo.Password = connectionSQL.Credential.Password.ToString();
+            string tempString = "";
+            //GetUser
+            int userBegin = x.IndexOf("user id=");
+            tempString = x.Substring(userBegin + 8);
+            int userEnd = tempString.IndexOf(";");
+            connectionInfo.User = tempString.Substring(0, userEnd);
+
+            //GetPassword
+            int passwordBegin = x.IndexOf("password=");
+            tempString = x.Substring(passwordBegin + 9);
+            int passwordEnd = tempString.IndexOf(";");
+            connectionInfo.Password = tempString.Substring(0, passwordEnd);
+
+            txtServer.Text = connectionInfo.Server;
+            txtDatabase.Text = connectionInfo.Database;
+            txtUser.Text = connectionInfo.User;
+            txtPassword.Text = connectionInfo.Password;
+
         }
 
+        private void SaveConfig()
+        {
+            SetupModels.DatabaseSetup databaseModel = new SetupModels.DatabaseSetup();
+            databaseModel.Server = txtServer.Text;
+            databaseModel.DatabaseName = txtDatabase.Text;
+            databaseModel.User = txtUser.Text;
+            databaseModel.Password = txtPassword.Text;
+            DynThings.Core.Database.SetDatabaseConnection(databaseModel);
+        }
+
+        private void ValidateServiceStatus()
+        {
+            EnableAll();
+            try
+            {
+                ServiceController sc = new ServiceController(ServiceName);
+
+                lblServiceStatus.Text = sc.Status.ToString();
+                btnService.Enabled = true;
+                switch (sc.Status)
+                {
+                    case ServiceControllerStatus.Running:
+                        lblServiceStatus.Text = "Runing";
+                        break;
+                    case ServiceControllerStatus.Stopped:
+
+                        break;
+                    case ServiceControllerStatus.Paused:
+
+                        break;
+                    case ServiceControllerStatus.StopPending:
+
+                        break;
+                    case ServiceControllerStatus.StartPending:
+
+                        break;
+                    default:
+
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblServiceStatus.Text = "Service is not Installed";
+                DisableAll();
+            }
+
+        }
+
+        private void DisableAll()
+        {
+            btnSave.Enabled = false;
+            btnService.Enabled = false;
+            txtServer.Enabled = false;
+            txtDatabase.Enabled = false;
+            txtUser.Enabled = false;
+            txtPassword.Enabled = false;
+        }
+
+        private void EnableAll()
+        {
+            btnSave.Enabled = true;
+            btnService.Enabled = true;
+            txtServer.Enabled = true;
+            txtDatabase.Enabled = true;
+            txtUser.Enabled = true;
+            txtPassword.Enabled = true;
+        }
     }
 }
