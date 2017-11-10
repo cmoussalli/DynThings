57702be97a19c1464403dedf5349a85e2db9c3a6
cmoussalli <cmoussalli@gmail.com>
2017-11-10 13:18:52 +0400

    Add Enums: ObjectStatus

---
+++ DynThings.Core/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynThings.Core
+{
+    public enum ObjectStatus
+    {
+        Disabled = 0,
+        Enabled = 1,
+        Deleted = 2,
+    }
+}
